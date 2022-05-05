f4150ed382b016e2a6f3dd3f1eb3e83951cf08c8
Lucas Kennedy <lukakfp@gmail.com>
2022-05-05 12:36:35 -0300

    produto modelo

---
+++ src/main/java/com/kintsugi/consumer/models/Product.java
@@ -0,0 +1,36 @@
+package com.kintsugi.consumer.models;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private int quantidade;
+
+    private LocalDate data;
+
+}
